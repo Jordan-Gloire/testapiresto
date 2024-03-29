92ad85b7869371eba55e3a08db8acd0155d94fda
Jordan Gloire <132207668+Jordan-Gloire@users.noreply.github.com>
2024-03-30 00:05:19 +0100

    Add files via upload

---
+++ page.js
@@ -0,0 +1,9 @@
+export default function Entete () {
+    return (
+        <>
+            <div className="bg-white shadow-lg my-4">
+                <h1 className="text-3xl font-bold pb-6 mx-5">mesRecettes</h1>
+            </div>
+        </>
+    )
+}
